a44093e5ab0e29e9b7f91f0e0c95f61a3b24abc5
anthonydavid3110 <anthony_31-10@hotmail.com>
2023-01-18 00:42:03 +0100

    check step 5

--- docker-images/apache-php-image/content/js/persons.js
+++ docker-images/apache-php-image/content/js/persons.js
@@ -6,7 +6,10 @@ $(function() {
             console.log(persons);
             var message = "Nobody is here";
             if (persons.length > 0) {
-                message = persons[0].firstName + " " + persons[0].lastName;
+                message = persons[0].firstName + " " 
+                          + persons[0].lastName + ", "
+                          + persons[0].street + " " + persons[0].number + ", "
+                          + persons[0].zip + " " + persons[0].city;
             }
             $( "#persons" ).text( message );
         });
